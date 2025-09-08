f79af88e6d38359bddaa419cec7c515897359fde
Berihuete Nunez, Engel <mastering23@gmail.com>
2025-09-08 17:57:41 -0400

    adding variables and CSS

--- src/components/MyAwesomeApp.tsx
+++ src/components/MyAwesomeApp.tsx
@@ -1,9 +1,54 @@
-export function MyAwesomeApp(){
+import type { CSSProperties } from "react";
+
+export const MyAwesomeApp = () =>{
+
+   const name1 = "David";
+   const name2 = "Bruno";
+
+   const foodTopics = ['Chesses','Tomato','Salsa','Pinapple'];
+
+   const isActive = true;
+
+   const address = {
+    zipCode: '10040',
+    country: 'USA',
+   }
+
+   const myStyles: CSSProperties = {
+      backgroundColor: '#9c1515ff',
+      color: 'white',
+      borderRadius: 10,
+      padding:20,
+      
+   }
 
   return(
     <>
      <h1>Creando un Nuevo Componente</h1>
      <h2>Creating a new Component</h2>
+
+     {/* Variables  */}
+     <h2>{name1}</h2>
+     <h2>{name2}</h2>
+
+    {/* suma */}
+    {2+2}
+
+    {/* arreglos  */}
+     <p>{foodTopics.join(', ')}</p>
+     
+    {/* ternarios  */}
+     <h2>{isActive ? 'Activo' : 'No Activo'}</h2>
+
+     {/* object  + css */}
+     <p
+     style={myStyles}
+     >
+
+    {JSON.stringify(address)}
+
+     </p>
+
     </>
 
   );
