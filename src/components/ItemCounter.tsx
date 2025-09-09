255c02d813ea26e823accdd9fd84e8df5e228b0c
Berihuete Nunez, Engel <mastering23@gmail.com>
2025-09-08 21:39:40 -0400

    adding shopping cart first structure

---
+++ src/components/ItemCounter.tsx
@@ -0,0 +1,24 @@
+interface Props {
+  name: string;
+  quantity?:number;
+}
+export const ItemCounter = ({ name , quantity}: Props) => {
+  return (
+    <section
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        gap: 10,
+      }}>
+      <span style={{
+        width: 150,
+        marginTop: 20,
+      }}>{name}</span>
+      <button>+1</button>
+      <span>{quantity}</span>
+      <button>-1</button>
+    </section>
+
+  );
+
+}
