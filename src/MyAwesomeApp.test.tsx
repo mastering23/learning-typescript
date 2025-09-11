76a7f0a53bf3f86293a6daecc79ebc440ccee558
Berihuete Nunez, Engel <mastering23@gmail.com>
2025-09-11 11:35:16 -0400

    finding elements in render  components and apply unit test

--- src/MyAwesomeApp.test.tsx
+++ src/MyAwesomeApp.test.tsx
@@ -1,7 +1,7 @@
-import { describe, test } from "vitest";
+import { describe, expect, test } from "vitest";
 import { MyAwesomeApp } from "./components/MyAwesomeApp";
 import {render , screen} from "@testing-library/react";
-import { print } from "./helpers/math.helper";
+// import { print } from "./helpers/math.helper";
 
 describe('MyAwesomeApp',() =>{
 
@@ -9,6 +9,14 @@ test('Should render firstName and lastName' , () =>{
 const {container} = render( <MyAwesomeApp />)
 // print(container.innerHTML);
 screen.debug();
+
+
+const h1 = container.querySelector('h1');
+const h3 = container.querySelector('h3');
+
+expect(h1?.innerHTML).toContain('Fernando');
+expect(h3?.innerHTML).toContain('Herrera');
+
 });
 
 });
